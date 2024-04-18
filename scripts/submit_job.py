e5d5e1a1065665fe35355313af870dbe990cf2bf
Yexuan Yang <myemailyyxg@gmail.com>
2024-04-18 16:16:26 +0000

    update submit_job

--- scripts/submit_job.py
+++ scripts/submit_job.py
@@ -2,7 +2,9 @@
 import yaml
 import xmlrpc.client
 
-with open('../tests/basic.yaml') as f:
+path = '/root/my_pipeline/tests/basic.yaml'
+
+with open(path) as f:
     config = yaml.dump(yaml.load(f, yaml.FullLoader))
     server=xmlrpc.client.ServerProxy("http://admin:longrandomtokenadmin@10.161.28.28:9999/RPC2/")
     jobid=server.scheduler.submit_job(config)
