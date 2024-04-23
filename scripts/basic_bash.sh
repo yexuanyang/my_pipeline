d75838206f08411b58ff3584f8841a00f7ebd63c
Yang Yexuan <1182282462@qq.com>
2024-04-23 22:43:58 +0800

    improve architecture

--- scripts/basic_bash.sh
+++ scripts/basic_bash.sh
@@ -1,3 +1,4 @@
 cd /root
+ls
 libevl
 ls
--- scripts/submit_job.py
+++ scripts/submit_job.py
@@ -1,10 +1,24 @@
 # Python3
 import yaml
 import xmlrpc.client
+import os
 
-path = '/root/my_pipeline/tests/basic.yaml'
+basic_job_dir = '../tests/jobs_defination/basic_job'
+libevl_job_dir = '../tests/jobs_defination/libevl_test'
+rpc_url = 'http://admin:longrandomtokenadmin@10.161.28.28:9999/RPC2/'
 
-with open(path) as f:
-    config = yaml.dump(yaml.load(f, yaml.FullLoader))
-    server=xmlrpc.client.ServerProxy("http://admin:longrandomtokenadmin@10.161.28.28:9999/RPC2/")
-    jobid=server.scheduler.submit_job(config)
+def submit_single_job(job_path, rpc_url):
+    with open(job_path) as f:
+        config = yaml.dump(yaml.load(f, yaml.FullLoader))
+        server=xmlrpc.client.ServerProxy(rpc_url)
+        jobid=server.scheduler.submit_job(config)
+        print("submit job " + str(jobid))
+
+def submit_all_jobs_in_directory(directory, rpc_url):
+    for root, dirs, files in os.walk(directory):
+        for file_name in files: 
+            file_path = os.path.join(root, file_name)
+            submit_single_job(file_path, rpc_url)
+
+submit_all_jobs_in_directory(basic_job_dir, rpc_url)
+submit_all_jobs_in_directory(libevl_job_dir, rpc_url)
