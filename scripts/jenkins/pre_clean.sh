58a42829a95de0308b142ba3f57c799eeac72b6d
yexuanyang <1182282462@qq.com>
2024-11-01 22:27:49 +0800

    Add a template `pre_clean.sh`

--- scripts/jenkins/pre_clean.sh
+++ scripts/jenkins/pre_clean.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-echo "hello, i am cancel_job"
 echo "pr_number is ${pr_number}"
 echo "last_build is ${last_build}"
 echo "multi is ${multi}"
+# prefix=""
+# normal_jobs="$prefix/pr-${pr_number}/${last_build}/jobs"
+# addition_jobs="$prefix/pr-${pr_number}/test-latest/jobs"
+echo "start cleaning addition_jobs"
+# 删除addition_jobs
+echo "finish cleaning addition_jobs"
 if [ ${multi} == "true" ]; then
-    echo "multi is true"
-else
-    echo "multi is false"
+    echo "start cleaning normal_jobs"
+    # 删除normal_jobs
+    echo "finfish cleaning normal_jobs"
 fi
