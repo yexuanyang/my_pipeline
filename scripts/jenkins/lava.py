7bd0c56d1f5b8738614659836c341adbf0cb5167
JiajunDu <wonderboy512@163.com>
2024-11-28 00:04:41 +0800

    add a function to check whether the lava task fails
    Signed-off-by: JiajunDu <wonderboy512@163.com>

--- scripts/jenkins/lava.py
+++ scripts/jenkins/lava.py
@@ -2,25 +2,65 @@ import xmlrpc.client
 import sys
 import os
 import time
+import requests
+from lxml import html
 from robot import post_rbot
 
 lava_rpc_url = 'http://admin:longrandomtokenadmin@10.161.28.28:9999/RPC2/'
 server = xmlrpc.client.ServerProxy(lava_rpc_url)
 job_id_dir_path = "/lava_jobs/rros/{}"
 
+def scrape_data(url, xpath):
+    try:
+        # 获取网页内容
+        response = requests.get(url)
+        response.raise_for_status()  # 检查请求是否成功
+
+        # 使用 lxml.html 解析 HTML 内容
+        tree = html.fromstring(response.content)
+
+        # 使用 XPath 提取所需的元素
+        elements = tree.xpath(xpath)
+        
+        # 提取元素内容并返回
+        data = [element.text_content() for element in elements]
+        return data
+    except requests.exceptions.RequestException as e:
+        print(f"HTTP request error: {e}")
+    except Exception as e:
+        print(f"Parse error: {e}")
+
+def has_fail(job_id: str):
+    # 示例URL和XPath
+    url = "http://10.161.28.28:9999/results/{}".format(job_id)
+    xpath = '//*[@id="table"]/tbody/tr/td[6]'
+    extracted_data = scrape_data(url, xpath)
+    fail_number = 0
+
+    if extracted_data:
+        for item in extracted_data:
+            fail_number += int(item)
+            print("has_fail: fail_item {}".format(item))
+    else:
+        print("Access {} with {} error, content is not found".format(url, xpath))
+    return fail_number == 0
+
 def polling_lava_result(jobs: list) -> dict:
-    # TODO: 可以设置一个最长的等待时间
     jobs_results = {}
     jobs_set = {id for id in jobs}
     while len(jobs_set) != 0:
         time.sleep(10)
         next_set = jobs_set.copy()
         for id in jobs_set:
-            state = server.scheduler.job_state(id)['job_state']
-            if state == 'Finished':
+            state = server.scheduler.job_health(id)['job_health']
+            if state == 'Complete':
                 next_set.remove(id)
-                result = server.scheduler.job_output(id).data.decode('utf-8')
-                jobs_results['id'] = result
+                if not has_fail(id):
+                    result = server.scheduler.job_output(id).data.decode('utf-8')
+                    jobs_results['id'] = result
+                else:
+                    # TODO: 表格里数据写成fail
+                    pass
             else:
                 break
         jobs_set = next_set
